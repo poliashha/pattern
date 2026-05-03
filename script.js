77b9ea6a19a0d6180d45192365800cc24162db4e
Polina Spirina <polly_sp@mail.ru>
2026-05-04 02:53:13 +0300

    video

--- script.js
+++ script.js
@@ -85,3 +85,38 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }, 6800); // 6.8 секунд
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".conteiner-video");
+
+  if (container) {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    container.addEventListener("mousedown", (e) => {
+      isDown = true;
+      container.style.cursor = "grabbing";
+      startX = e.pageX - container.offsetLeft;
+      scrollLeft = container.scrollLeft;
+    });
+
+    container.addEventListener("mouseleave", () => {
+      isDown = false;
+      container.style.cursor = "grab";
+    });
+
+    container.addEventListener("mouseup", () => {
+      isDown = false;
+      container.style.cursor = "grab";
+    });
+
+    container.addEventListener("mousemove", (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - container.offsetLeft;
+      const walk = (x - startX) * 2;
+      container.scrollLeft = scrollLeft - walk;
+    });
+  }
+});
